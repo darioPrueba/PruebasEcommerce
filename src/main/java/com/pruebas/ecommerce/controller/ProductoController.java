9aa7f312e1d53c633946e9deb628c7bebd5d6c04
Dario <dariomsn@yahoo.com.ar>
2021-11-26 13:16:27 -0300

    se creó la función para guardar productos y su respectiva vista

--- src/main/java/com/pruebas/ecommerce/controller/ProductoController.java
+++ src/main/java/com/pruebas/ecommerce/controller/ProductoController.java
@@ -1,13 +1,25 @@
 package com.pruebas.ecommerce.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.pruebas.ecommerce.model.Producto;
+import com.pruebas.ecommerce.model.Usuario;
+import com.pruebas.ecommerce.service.ProductoService;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
 
+	private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
+	@Autowired
+	private ProductoService productoService;
 	@GetMapping("")
 	public String show() {
 		
@@ -19,6 +31,16 @@ public class ProductoController {
 	public String create() {
 		
 		return "productos/create";
+	}
+
+	@PostMapping("/save")
+	public String save(Producto producto) {
+
+		LOGGER.info("Este es el objeto producto {}", producto);
+		Usuario u = new Usuario (1, null, null, null, null, null, null, null);
+		producto.setUsuario(u);
+		productoService.save(producto);
+		return "redirect:/productos";
 	}
 	
 	
