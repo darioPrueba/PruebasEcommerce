2d5054681da0c0192ac3ccb101fd8d5907c417bf
Dario <dariomsn@yahoo.com.ar>
2021-11-29 14:51:54 -0300

    Creada la funcionalidad para eliminar un producto

--- src/main/java/com/pruebas/ecommerce/controller/ProductoController.java
+++ src/main/java/com/pruebas/ecommerce/controller/ProductoController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -71,4 +72,15 @@ public class ProductoController {
 		
 		return "redirect:/productos";
 	}
+	
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable Integer id) {
+
+		productoService.delete(id);
+		return "redirect:/productos";
+		
+		
+	}
+	
+	
 }
