f7588b02cef3fdab072587e6192eca0930c84c40
Dario <dariomsn@yahoo.com.ar>
2021-12-06 16:38:53 -0300

    Comencé con el carrito

--- src/main/java/com/pruebas/ecommerce/controller/HomeController.java
+++ src/main/java/com/pruebas/ecommerce/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pruebas.ecommerce.model.Producto;
@@ -41,4 +42,15 @@ public class HomeController {
 		return "usuario/productohome";
 		
 	}
+	
+	@PostMapping("/cart")
+	public String addCart() {
+		
+		
+		return "usuario/carrito";
+	}
+	
+	
+	
+	
 }
