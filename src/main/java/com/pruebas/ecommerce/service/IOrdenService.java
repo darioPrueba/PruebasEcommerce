3b19b9c1a847d1eedb6999706f5a8e3b7b82223e
Dario <dariomsn@yahoo.com.ar>
2021-12-10 15:29:26 -0300

    Creamos el metodo que genera el numero de la orden

--- src/main/java/com/pruebas/ecommerce/service/IOrdenService.java
+++ src/main/java/com/pruebas/ecommerce/service/IOrdenService.java
@@ -1,9 +1,13 @@
 package com.pruebas.ecommerce.service;
 
+import java.util.List;
+
 import com.pruebas.ecommerce.model.Orden;
 
 public interface IOrdenService {
 
+	public List <Orden> findAll();
 	public Orden save (Orden orden);
+	
 	
 }
