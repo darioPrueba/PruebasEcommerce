26e92c5dc48f9212f08f02038404de82126410fe
Dario <dariomsn@yahoo.com.ar>
2022-05-17 09:06:31 -0300

    Final commit for 1st stage

--- src/main/java/com/pruebas/ecommerce/controller/userController.java
+++ src/main/java/com/pruebas/ecommerce/controller/userController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,6 +33,8 @@ public class userController {
 	@Autowired
 	private IOrdenService ordenService;
 	
+	BCryptPasswordEncoder passEncode = new BCryptPasswordEncoder();
+	
 	@GetMapping("/registro")
 	public String create() {
 				
@@ -44,6 +47,7 @@ public class userController {
 	LOG.info("Usuario registro: {}", usuario);
 
 	usuario.setTipo("USER");
+	usuario.setPassword(passEncode.encode(usuario.getPassword()));
 	usuarioService.save(usuario);
 	return "redirect:/";
 	}
@@ -54,11 +58,11 @@ public class userController {
 		return "usuario/login";
 	}
 
-	@PostMapping("/acceder")
+	@GetMapping("/acceder")
 	public String acceder(Usuario usuario, HttpSession session) {
 
 		LOG.info("Accesos : {}", usuario);		
-		Optional<Usuario> user = usuarioService.findByEmail(usuario.getEmail());
+		Optional<Usuario> user = usuarioService.findById(Integer.parseInt(session.getAttribute("idusuario").toString()));
 		LOG.info("Usuario de base de datos: {}", user.get());	
 		
 		if(user.isPresent()) {
